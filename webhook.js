801ce075a2464ae853a5a308e8d7b2ee87697b1e
YOONBYEONGIN <rpf5573@gmail.com>
2019-02-25 16:02:39 +0900

    t03

--- webhook.js
+++ webhook.js
@@ -6,6 +6,7 @@ let crypto = require('crypto');
 const exec = require('child_process').exec;
 
 http.createServer(function(req, res){
+  console.log( 'webhook.js is running : ' );
   req.on('data', function(chunk){
     let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
     if (req.headers['x-hub-signature'] == sig) {
