34de7ac7948ae7fd750331e9339a78ec339e5447
YOONBYEONGIN <rpf5573@gmail.com>
2019-02-25 15:10:50 +0900

    Listen

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ app.use(bodyParser.json());
 app.post('/led', (req, res) => {
   return res.status(201).send({ message: 'hey' });
 });
+
+var server = require('http').Server(app);
+let PORT = 8081;
+server.listen(PORT, 'localhost');
