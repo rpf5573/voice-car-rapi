893ff1f3f4b2b17380e1e807120079c61fa65ba0
YOONBYEONGIN <rpf5573@gmail.com>
2019-03-07 12:29:09 +0900

    test06

--- sm.js
+++ sm.js
@@ -2,9 +2,10 @@ const Gpio = require('pigpio').Gpio;
 
 const HIGH = 1;
 const LOW = 0;
+const dutyCycle = 230;
 
 const in1 = new Gpio(4, {mode: Gpio.OUTPUT});
 const in2 = new Gpio(17, {mode: Gpio.OUTPUT});
 
-in1.digitalWrite(HIGH);
-in2.digitalWrite(LOW);
+in1.pwmWrite(dutyCycle);
+in2.pwmWrite(dutyCycle);
