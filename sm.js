57888287a1ad061c8f1b3ec7e411c570b3be3480
YOONBYEONGIN <rpf5573@gmail.com>
2019-03-07 12:57:26 +0900

    test08

--- sm.js
+++ sm.js
@@ -4,11 +4,24 @@ const HIGH = 1;
 const LOW = 0;
 const dutyCycle = 230;
 
-const in1 = new Gpio(4, {mode: Gpio.OUTPUT});
-const in2 = new Gpio(17, {mode: Gpio.OUTPUT});
+const in1 = new Gpio(4, {
+	mode: Gpio.OUTPUT
+});
+const in2 = new Gpio(17, {
+	mode: Gpio.OUTPUT
+});
 
 // in1.pwmWrite(dutyCycle);
 // in2.pwmWrite(dutyCycle);
 
 in1.digitalWrite(HIGH);
 in2.digitalWrite(LOW);
+
+process.on("SIGINT", function(){
+
+	in1.digitalWrite(LOW);
+	in2.digitalWrite(LOW);
+
+  console.log('shutdown!');
+  process.exit(0);
+});
