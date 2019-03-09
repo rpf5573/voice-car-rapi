c3c6fa17e2dc36503b35531b02bf5693339eca8e
YOONBYEONGIN <rpf5573@gmail.com>
2019-03-09 11:16:34 +0900

    motor setting

--- index.js
+++ index.js
@@ -11,18 +11,23 @@ let l298n = new L298N(17,27,22,null,null,null);
 l298n.setSpeed(l298n.NO1,80);
 
 app.get('/forward', async (req, res) => {
+  l298n.forward(l298n.NO1);
   return res.status(201).send({ message: '알았어 앞으로 갈께' });
 });
 
 app.get('/backward', async (req, res) => {
+  l298n.backward(l298n.NO1);
   return res.status(201).send({ message: '알았어 뒤로 갈께' });
+});
+
+app.get('/stop', async (req, res) => {
+  l298n.stop(l298n.NO1);
+  return res.status(201).send({ message: '우리이제 그만하자...' });
 });
 
 app.get('/', (req, res) => {
   return res.status(201).send({ message: '여긴 왜왔어? 왜 자꾸와?? 니가 자꾸 오면,,, 내가 기다리게 되잖아ㅠㅠ' });
 });
-
-console.log("car is moving !");
 
 var server = require('http').Server(app);
 let PORT = 8081;
